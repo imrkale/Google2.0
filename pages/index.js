dcbd4f9ebbbedc59b3216c83c1f416d35a168d9c
Raj Kale <kale44772@gmail.com>
2021-04-09 13:39:32 +0530

    body added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
+import Image from 'next/image'
 import {MicrophoneIcon,ViewGridIcon} from '@heroicons/react/solid'
+import {SearchIcon} from '@heroicons/react/outline'
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <Head>
         <title>Google Raj</title>
         <link rel="icon" href="/favicon.ico" />
@@ -24,7 +26,22 @@ export default function Home() {
           {/* https://www.thestatesman.com/wp-content/uploads/2020/09/EicwoqZUYAAxDUE.jpeg */}
        </div>
      </header>
+
      {/* Body */}
+     <form className="flex flex-col items-center mt-40">
+        <Image src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" width={300} height={100}/>
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg sm:max-w-xl lg:max-w-2xl max-w-md place-items-center rounded-full items-center border border-gray-200">
+          <SearchIcon className="h-5 mr-3 text-gray-500 m-2"/>
+          <input type="text" className="outline-none flex-grow p-2"/>
+          <MicrophoneIcon className="h-5 m-3 text-gray-500"/>
+        </div>
+        <div className="">
+            <button className="btn">Google Search</button>
+            <button className="btn">I'm Feeling Lucky</button>
+        </div>
+     </form>
+     
+
     {/* Footer */}
     </div>
   )
